5a74ef1057b63913dc32ec36e0c6ac511b5f035d
Bhargavwelcome <160607599+Bhargavwelcome@users.noreply.github.com>
2025-11-12 10:57:40 +0530

    Add files via upload

---
+++ webpush-service-worker (2).js
@@ -0,0 +1,3 @@
+importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
+var config = '{"appserviceKey":"BE_QYsSpUjlPflz66YjLtxez2s8Gl2PC91Ox_QHuHSWUOZLrYbrBmhZiuYBbLLLYGw-YsKpoq7Q0WdY74kfNIqY=","apiKey":"ABEOzqUHvCM5ly5OJxkX7p6YY","accountToken":"ABElKaqpK-3l_zDeOp-XArlkg","appver":"0.0.0","apiHost":"https://abr16c0-webpush.oraclersys.com","lazy":false}';
+orawp.init(config);
